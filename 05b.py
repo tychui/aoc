0d6ebbb3f924f50ae8a8e287e42513d79663fa79
tychui <88720874+tychui@users.noreply.github.com>
2021-12-06 06:37:02 +0000

    temp 05b

--- 05b.py
+++ 05b.py
@@ -1,1 +1,9 @@
+def lantern(n, day):
+    count = 0
+    return count
 
+lanternfish = list(map(int, input().split(',')))
+count = 0
+for i in range(len(lanternfish)):
+    count += lantern(lanternfish[i], 0)
+print(count)
