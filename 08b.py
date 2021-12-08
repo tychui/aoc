839e931f66375ac910d1bf2a543fd22cb90a0315
tychui <88720874+tychui@users.noreply.github.com>
2021-12-08 08:22:38 +0000

    Update 08b.py

--- 08b.py
+++ 08b.py
@@ -9,13 +9,13 @@ def pattern(patternlst):
             patlst[4] = "".join(sorted(patternlst[i]))
         elif len(patternlst[i]) == 7:
             patlst[8] = "".join(sorted(patternlst[i]))
-        if patlst[1] != '':
-            if len(patternlst[i]) == 6 and (patlst[1][0] not in patternlst[i] or patlst[1][1] not in patternlst[i]):
-                patlst[6] = "".join(sorted(patternlst[i]))
-            elif len(patternlst[i]) == 5 and (patlst[1][0] in patternlst[i] and patlst[1][1] in patternlst[i]):
-                patlst[3] = "".join(sorted(patternlst[i]))
+    for i in range(len(patternlst)):
+        if "".join(sorted(patternlst[i])) in patlst:
+            continue
         elif len(patternlst[i]) == 6:
-            if patlst[4] != '':
+            if patlst[1][0] not in patternlst[i] or patlst[1][1] not in patternlst[i]:
+                patlst[6] = "".join(sorted(patternlst[i]))
+            else:
                 status = True
                 for j in range (len(patlst[4])):
                     if patlst[4][j] not in patternlst[i]:
@@ -24,14 +24,35 @@ def pattern(patternlst):
                     patlst[9] = "".join(sorted(patternlst[i])) 
                 else:
                     patlst[0] = "".join(sorted(patternlst[i]))
+    for i in range(len(patternlst)):
+        patternlst[i] = "".join(sorted(patternlst[i]))
+        status = True
+        for j in range (len(patlst)):
+            if patternlst[i] != patlst[j]:
+                status = False
+        if status == True:
+            continue
         elif len(patternlst[i]) == 5:
-            if patlst[1] != '':
-                total = "".join(patlst[1],patternlst[i])
-                total = "".join(sorted(total))
+            if (patlst[1][0] in patternlst[i]) and (patlst[1][1] in patternlst[i]):
+                patlst[3] = "".join(sorted(patternlst[i]))
+            else:
+                total = []
+                for j in range(len(patternlst[i])):
+                    total.append(patternlst[i][j])
+                for j in range(len(patlst[1])):
+                    total.append(patlst[1][j])
+                total.sort()
+                k = 0
                 for j in range(1, len(total)):
                     if total[j] == total[j-1]:
-                        total.remove(total[j])
-                if total == patlst[9]:
+                        k = j-1
+                total.remove(total[k])
+                total = "".join(total)
+                statusn = True
+                for j in range(len(total)):
+                    if total[j] != patlst[9][j]:
+                        statusn = False
+                if statusn == True:
                     patlst[5] = "".join(sorted(patternlst[i]))
                 else:
                     patlst[2] = "".join(sorted(patternlst[i])) 
@@ -48,6 +69,6 @@ for i in range(200):
         outputlst[i] = "".join(sorted(outputlst[i]))
         for j in range (len(patternlst)):
             if outputlst[i] == patternlst[j]:
-                num += (10**(3-i))*(j+1)
+                num += (10**(3-i))*j
     total += num
 print(total)
